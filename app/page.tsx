ec745fde60421319c08006a1e80298788f925482
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-11-09 03:16:57 +0530

    Added timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
  "use client";
+
 import { About } from "@/pages/About";
 import { FloatingDockDemo } from "@/pages/FloatingDesk";
 import { Hero } from "@/pages/Hero";
+import { TimelineDemo } from "@/pages/Timeline";
 
 export default function Home() {
   return (
-    <div  > {/* Adds padding to the bottom to avoid overlap */}
-      <Hero />
-      <About />
-      <FloatingDockDemo />
+    <div>
+      {/* Wrapping each component in a div with margin bottom for spacing */}
+      <div className="mb-12">
+        <Hero />
+      </div>
+      <div className="mb-12">
+        <About />
+      </div>
+      <div className="mb-12">
+        <FloatingDockDemo />
+      </div>
+      <div className="mb-12">
+        <TimelineDemo />
+      </div>
     </div>
   );
 }
--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -38,13 +38,12 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          Changelog from my journey
+        <h2 className="text-lg md:text-7xl mb-4 text-black dark:text-white max-w-4xl">
+           My Experience.
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
-        </p>
+          I am a third year cs undergrad, A Passionate learner, and I have been contributing to open source for a year now.
+          </p>
       </div>
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
